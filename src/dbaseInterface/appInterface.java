9ffe41f1be7bc0252da9c4bdaba0195e2408824c
Hassan <phelenon@gmail.com>
2020-10-29 22:29:48 +0500

    Add database interface expection documentation

--- src/dbaseInterface/appInterface.java
+++ src/dbaseInterface/appInterface.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public interface appInterface
 {
+    //if app does not exist throw invalidargumentexception
     public appDetails getAppDetails(int appID);
 
     //return appIds of all apps
@@ -12,15 +13,22 @@ public interface appInterface
     //returns all appIds in the category
     public List<Integer> getAppsInCategory(String Category);
 
+    //if app does not exist throw invalidargumentexception
     public String getAppContent(int appID);
 
+    //if app, user does not exist throw invalidargumentexception
+    //rating passed should be between 0 and 5
     public void addRating(int appID, int userID, int rating);
 
+    //if app, user does not exist throw invalidargumentexception
     public void addComment(int appID, int userID, String comment);
 
+    //if app exists, throw invalidargumentexception
     public void addApp(appDetails app);
 
+    //if add does not exist, throw invalidargumentexception
     public void updateApp(appDetails app);
 
+    //if app does not exist, throw invalidargumentexception
     public void removeApp(int appID);
 }
--- src/dbaseInterface/userInterface.java
+++ src/dbaseInterface/userInterface.java
@@ -2,13 +2,18 @@ package dbaseInterface;
 
 public interface userInterface
 {
+    // if user not found raise invalidArgumentException
     public userDetails getUserDetails(int userID);
 
+    //if user exists raise invalidargumentexception
     public void addUser(userDetails user);
 
+    //if user does not exist throw invalidargumentexception
     public void removeUser(int userID);
 
+    //if user or app does not exists, throw invalidargumentexception
     public void addInstalledApp(int appID, int userID);
 
+    //if user or app does not exists, throw invalidargumentexception
     public void removeInstalledApp(int appID, int userID);
 }
