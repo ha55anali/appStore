b6560b63f2538db1150adac86f94d0cb77f57fef
Afaq Ahmad <afaq11072001@gmail.com>
2020-10-30 19:03:57 +0500

    Class FileCheck is implimented for use in all other classes for DB Handeling

--- src/dbaseInterface/User.java
+++ src/dbaseInterface/User.java
@@ -1,12 +1,15 @@
 package dbaseInterface;
 
 import java.io.*;
+import java.io.IOException;
 
 public class User {
     
     //made this main() to test the functions ill be making in this class
     public static void main(String[] args) {
         
-        
+        File test = new File("test123.txt");
+
+        System.out.println(FileCheck.CheckFile(test));
     }
 }
