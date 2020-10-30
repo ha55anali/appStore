7ed10c4d5f0e5fb0b85a8f008d5fa1f898f9ab27
Hassan <phelenon@gmail.com>
2020-10-30 20:33:49 +0500

    Add empty db layer factory

---
+++ src/dbaseInterface/dbFactory.java
@@ -0,0 +1,17 @@
+public class dbFactory {
+    public static appInterface getAppObject()
+    {
+        return null;
+    }
+
+    public static devInterface getDevObject()
+    {
+        return null;
+    }
+
+    public static userInterface getUserObject()
+    {
+        return null;
+    }
+    
+}
