2912c0201ac8e6ebdb6a1a4d154148c58195f547
Hassan <phelenon@gmail.com>
2020-10-30 20:38:37 +0500

    Add bl interface factory

---
+++ src/blInterface/blFactory.java
@@ -0,0 +1,21 @@
+import dbaseInterface.appInterface;
+import dbaseInterface.devInterface;
+import dbaseInterface.userInterface;
+
+public class blFactory
+{
+    public static appInterface getAppObject()
+    {
+        return null;
+    }
+
+    public static devInterface getDevObject()
+    {
+        return null;
+    }
+
+    public static userInterface getUserObject()
+    {
+        return null;
+    }
+}
