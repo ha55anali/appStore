1e642d30496e067016ca28d5def7d3e441f2c2eb
Nushirvan Naseer <nushirvannaseer@gmail.com>
2020-10-31 20:49:16 +0500

    changed the return type for getUserObject

--- src/businessLayer/blFactory.java
+++ src/businessLayer/blFactory.java
@@ -1,19 +1,15 @@
 package businessLayer;
 
-public class blFactory
-{
-    public static blInterface.appInterface getAppObject()
-    {
+public class blFactory {
+    public static blInterface.appInterface getAppObject() {
         return new App();
     }
 
-    public static blInterface.devInterface getDevObject()
-    {
+    public static blInterface.devInterface getDevObject() {
         return null;
     }
 
-    public static blInterface.userInterface getUserObject()
-    {
-        return null;
+    public static blInterface.userInterface getUserObject() {
+        return new User();
     }
 }
