2ecc3b20eb2b876f7785b1a8200f4540c6eb85a9
Hassan <phelenon@gmail.com>
2020-10-31 15:50:56 +0500

    BUGFIX: change location of business layer factory

---
+++ src/businessLayer/blFactory.java
@@ -0,0 +1,19 @@
+package businessLayer;
+
+public class blFactory
+{
+    public static blInterface.appInterface getAppObject()
+    {
+        return new App();
+    }
+
+    public static blInterface.devInterface getDevObject()
+    {
+        return null;
+    }
+
+    public static blInterface.userInterface getUserObject()
+    {
+        return null;
+    }
+}
