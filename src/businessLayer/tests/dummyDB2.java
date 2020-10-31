d9fe47fc5a8de59f511a65821c7b148d5f8f4a4d
Nushirvan Naseer <nushirvannaseer@gmail.com>
2020-10-31 23:35:28 +0500

    updated methods for dummy db

--- src/businessLayer/tests/dummyDB2.java
+++ src/businessLayer/tests/dummyDB2.java
@@ -1,9 +1,8 @@
 package businessLayer.tests;
 
-import dbaseInterface.*;
+import java.time.LocalDate;
 
 import dbaseInterface.*;
-import java.util.*;
 
 public class dummyDB2 implements dbaseInterface.userInterface {
 
@@ -15,19 +14,61 @@ public class dummyDB2 implements dbaseInterface.userInterface {
     }
 
     public userDetails getUserDetails(int userID) {
-        return null;
+        if (userID < 100)
+            return new userDetails("nushiPishi", 69, LocalDate.now(), "nushi@pishi.com", "yibz6969");
+        else
+            throw new IllegalArgumentException("alo jee error aya reee");
     }
 
     // if user does not exist throw invalidargumentexception
     public void removeUser(int userID) {
+        if (userID == 69)
+            return;
+        else
+            throw new IllegalArgumentException("alo jee ik hor error aya jay");
     }
 
     // if user or app does not exists, throw invalidargumentexception
     public void addInstalledApp(int appID, int userID) {
+        if (appID == 69)
+            throw new IllegalArgumentException("app already installed dumbo");
+        else if (userID != 69)
+            throw new IllegalArgumentException("User dont exist dumbas");
     }
 
     // if user or app does not exists, throw invalidargumentexception
     public void removeInstalledApp(int appID, int userID) {
+        if (appID != 69)
+            throw new IllegalArgumentException("app not installed dumbo");
+    }
+
+    // if user does not exist throw invalidargumentexception
+    public void authenticateUser(int userID) {
+        if (userID != 69)
+            throw new IllegalArgumentException("ni krna stfu");
+    }
+
+    public void addCard(int cardNo, int ExpYear) {
+        if (cardNo != 69)
+            throw new IllegalArgumentException("Nope cant add that card no");
+    }
+
+    // if user or app does not exists, throw invalidargumentexception
+    public void setPaymentMethod(String method) {
+        if (method != "dhui")
+            throw new IllegalArgumentException("dont accept this method");
+    }
+
+    // if user or app does not exists, throw invalidargumentexception
+    public void changeCardDetails(int cardNo, int NewExpYear) {
+        if (cardNo != 69)
+            throw new IllegalArgumentException("Nope cant change that card's deets");
+    }
+
+    // if user or app does not exists, throw invalidargumentexception
+    public void removeCardDetails(int cardNo) {
+        if (cardNo != 69)
+            throw new IllegalArgumentException("Nope nope nope");
     }
 
 }
---
+++ src/businessLayer/tests/dummyDB_REMOTE_20369.java
@@ -0,0 +1,71 @@
+package businessLayer.tests;
+
+import dbaseInterface.*;
+import java.util.*;
+
+public class dummyDB implements appInterface
+{
+    //if app does not exist throw invalidargumentexception
+    public appDetails getAppDetails(int appID)
+    {
+        if (appID == 5)
+            return new appDetails(1, 1, new ArrayList<Integer>(), 0, new ArrayList<String>() );
+        else if (appID == 10)
+            return new appDetails(2, 2, new ArrayList<Integer>(), 0, new ArrayList<String>() );
+        else
+            throw new IllegalArgumentException("hello");
+    }
+
+    //return appIds of all apps
+    public List<Integer> getAllApps()
+    {
+        List<Integer> a= new ArrayList<Integer>();
+        a.add(5);
+        a.add(10);
+        return a;
+    }
+
+    //returns all appIds in the category
+    public List<Integer> getAppsInCategory(String Category)
+    {
+        return null;
+    }
+
+    //if app does not exist throw invalidargumentexception
+    public String getAppContent(int appID)
+    {
+        return null;
+    }
+
+    //if app, user does not exist throw invalidargumentexception
+    //rating passed should be between 0 and 5
+    public void addRating(int appID, int userID, int rating)
+    {
+        
+    }
+
+    //if app, user does not exist throw invalidargumentexception
+    public void addComment(int appID, int userID, String comment)
+    {
+        
+    }
+
+    //if app exists, throw invalidargumentexception
+    public void addApp(appDetails app)
+    {
+        
+    }
+
+    //if add does not exist, throw invalidargumentexception
+    public void updateApp(appDetails app)
+    {
+        
+    }
+
+    //if app does not exist, throw invalidargumentexception
+    public void removeApp(int appID)
+    {
+        
+    }
+
+}
