a9575c5bb03db35b937d726d29a1477357e99e64
Nushirvan Naseer <nushirvannaseer@gmail.com>
2020-11-01 14:36:25 +0500

    add functions to db interface

--- src/dbaseInterface/userDetails.java
+++ src/dbaseInterface/userDetails.java
@@ -1,12 +1,27 @@
 package dbaseInterface;
 
 import java.time.LocalDate;
+import java.util.jar.Attributes.Name;
 
-public class userDetails
-{
+public class userDetails {
     public String Name;
     public int userID;
     public LocalDate DOB;
     public String password;
     public String email;
+
+    public userDetails() {
+        Name = null;
+        userID = -1;
+        DOB = null;
+        email = null;
+    }
+
+    public userDetails(String Name, int userID, LocalDate DOB, String email, String password) {
+        this.Name = Name;
+        this.userID = userID;
+        this.DOB = DOB;
+        this.email = email;
+        this.password = password;
+    }
 }
--- src/dbaseInterface/userInterface.java
+++ src/dbaseInterface/userInterface.java
@@ -1,19 +1,33 @@
 package dbaseInterface;
 
-public interface userInterface
-{
+public interface userInterface {
     // if user not found raise invalidArgumentException
     public userDetails getUserDetails(int userID);
 
-    //if user exists raise invalidargumentexception
+    // if user exists raise invalidargumentexception
     public void addUser(userDetails user);
 
-    //if user does not exist throw invalidargumentexception
+    // if user does not exist throw invalidargumentexception
     public void removeUser(int userID);
 
-    //if user or app does not exists, throw invalidargumentexception
-    public void addInstalledApp(int appID, int userID, int ver);
+    // if user does not exist throw invalidargumentexception
+    public void addCard(int cardNo, int ExpYear);
 
-    //if user or app does not exists, throw invalidargumentexception
+    // if user does not exist throw invalidargumentexception
+    public void authenticateUser(int userID);
+
+    // if user or app does not exists, throw invalidargumentexception
+    public void addInstalledApp(int appID, int userID);
+
+    // if user or app does not exists, throw invalidargumentexception
     public void removeInstalledApp(int appID, int userID);
+
+    // if user or app does not exists, throw invalidargumentexception
+    public void setPaymentMethod(String method);
+
+    // if user or app does not exists, throw invalidargumentexception
+    public void changeCardDetails(int cardNo, int NewExpYear);
+
+    // if user or app does not exists, throw invalidargumentexception
+    public void removeCardDetails(int cardNo);
 }
