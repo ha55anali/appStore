031a97ea169b4d0948549c892d2e1fc3122f753d
Hassan <phelenon@gmail.com>
2020-11-01 15:24:22 +0500

    update interface to switch validation to business layer

--- src/dbaseInterface/userInterface.java
+++ src/dbaseInterface/userInterface.java
@@ -1,19 +1,24 @@
 package dbaseInterface;
 
+//assume all parameters passed are correct
 public interface userInterface
 {
-    // if user not found raise invalidArgumentException
     public userDetails getUserDetails(int userID);
 
-    //if user exists raise invalidargumentexception
+    public boolean checkUserExists(int userID);
+
     public void addUser(userDetails user);
 
-    //if user does not exist throw invalidargumentexception
     public void removeUser(int userID);
 
-    //if user or app does not exists, throw invalidargumentexception
     public void addInstalledApp(int appID, int userID, int ver);
 
-    //if user or app does not exists, throw invalidargumentexception
     public void removeInstalledApp(int appID, int userID);
+
+    //return app version
+    // -1 if not installed
+    public int checkAppInstall(int appID, int userID);
+
+    //return 1 if email is being used by some user
+    public boolean checkEmailExists(String email);
 }
