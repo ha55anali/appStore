238200c3e79d06cd8d4d1ee52935ad4246dd5aa4
Afaq Ahmad <afaq11072001@gmail.com>
2020-11-08 19:13:28 +0500

    removeUser() implimented

--- src/dbaseInterface/User.java
+++ src/dbaseInterface/User.java
@@ -144,13 +144,119 @@ public class User {
         return details;
 
     }
+    
+    public void removeUser(int userID) {
+
+        //checking if ANY user exists or not
+        File count_file = new File("totalUsers.txt");
+        int file_status = FileControl.CheckFile(count_file);
+
+        if(file_status == -1)   //file wasn't present earlier
+            return;
+        else if(file_status == 1)       //a new file is made
+        {
+            count_file.delete();
+            return;
+        }        
+
+        //now we know that file exists
+        //up to the real work now. Reading file to find the userID ughh
+
+        File user_file = new File("user_record.txt");
+        file_status = FileControl.CheckFile(user_file);
+
+        if(file_status == -1)       //couldn't open file or file was't present in first place
+            return;
+        else if(file_status == 1) {
+            user_file.delete();
+            return;
+        }
+        
+        //now we might find the user and have to re-write the file
+        //for that, lets opy all the users in a list first
+
+        List<userDetails> temp_list = new LinkedList<userDetails>();
+        boolean userFound = false;
+
+        try {
+            Scanner scanner = new Scanner(user_file);
+            while(scanner.hasNextLine()) {              //will read the file till end
+                
+                userDetails temp_user = new userDetails();
+
+                String data = scanner.nextLine();
+                try {
+                    if(Integer.valueOf(data) != userID) {
+
+                        temp_user.userID = Integer.valueOf(data);               //its not the user which is to be deleted, hence storing
+                        temp_user.Name = scanner.nextLine();                    //next line has name, so stored
+                        temp_user.DOB = LocalDate.parse(scanner.nextLine());    //date is converted from String to LocalDate type to store back in returning obj
+                        temp_user.password = scanner.nextLine();
+                        temp_user.email = scanner.nextLine();
+
+                        temp_list.add(temp_user);
+                    }
+
+                    else {      //user deleted to be. Hence, discarding
+
+                        userFound = true;
+
+                        scanner.nextLine();     //name discarded
+                        scanner.nextLine();     //DOB discarded
+                        scanner.nextLine();     //password discarded
+                        scanner.nextLine();     //email discarded
+                    }
+                }
+                catch (Exception ex) {
+                    continue;
+                }
+            }
+            scanner.close();
+        }
+        catch (Exception e) {
+
+            System.out.println("An error occured in writing user file\n");
+            e.printStackTrace();
+        }
+
+        if(!userFound)
+            return;
+
+        //time to write back in file
+
+        try {
+            FileWriter writer = new FileWriter(user_file);
+
+            int total_users = temp_list.size();
+
+            for(int i=0; i<total_users; i++) {
+                
+                userDetails temp_user = new userDetails();
+                temp_user = temp_list.remove(0);
+
+                writer.write(String.valueOf(temp_user.userID) + "\n");
+                writer.write(temp_user.Name + "\n");
+                writer.write(String.valueOf(temp_user.DOB) + "\n");
+                writer.write(temp_user.password + "\n");
+                writer.write(temp_user.email + "\n");
+            }
+
+            writer.close();
+        }
+        catch (Exception e) {
+
+            System.out.println("An error occured in writing user file in delete method\n");
+            e.printStackTrace();
+        }
+    }
+    
     // made this main() to test the functions ill be making in this class
     public static void main(String[] args) {
 
         // LocalDate dobby = LocalDate.now();
-        //User testing = new User();
+        // User testing = new User();
         // userDetails obj = new userDetails("Afaq", 13233, dobby, "afaq@gmail", "nooo");
-        // testing.addUser(obj);
+        //testing.addUser(obj);
 
         // for(int i=1; i<9; i++) {
         //     userDetails obj = testing.getUserDetails(i);
@@ -160,5 +266,7 @@ public class User {
         //     else
         //         System.out.println("\nUSER NOT FOUND\n");
         // }
+
+        //testing.removeUser(3);
     }
 }
