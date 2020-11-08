60bdf9b7ed60db8e0c4e84d3b89ba6064df6559e
Nushirvan Naseer <nushirvannaseer@gmail.com>
2020-11-08 12:45:34 +0500

    testing file. call functions here to test

--- src/test.java
+++ src/test.java
@@ -13,62 +13,14 @@ class test {
     public static void main(String[] args) {
 
         blInterface.userInterface user = new businessLayer.User();
-        blInterface.userDetails deets = new blInterface.userDetails("Nushi", 1, LocalDate.now(), "nushi@pishi.com",
-                "pishipishi");
-        user.addUser(deets);
-
-        // Connection conn = null;
-
-        // try {
-
-        // String dbURL = "jdbc:sqlserver://localhost\\master:1433";
-        // String user = "sa";
-        // String pass = "Yibz6969";
-        // conn = DriverManager.getConnection(dbURL, user, pass);
-        // if (conn != null) {
-        // DatabaseMetaData dm = (DatabaseMetaData) conn.getMetaData();
-        // System.out.println("Driver name: " + dm.getDriverName());
-        // System.out.println("Driver version: " + dm.getDriverVersion());
-        // System.out.println("Product name: " + dm.getDatabaseProductName());
-        // System.out.println("Product version: " + dm.getDatabaseProductVersion());
-        // }
-
-        // } catch (SQLException ex) {
-        // ex.printStackTrace();
-        // } finally {
-        // try {
-        // if (conn != null && !conn.isClosed()) {
-        // conn.close();
-        // }
-        // } catch (SQLException ex) {
-        // ex.printStackTrace();
-        // }
-        // }
-
-        // String dbURL =
-        // "jdbc:sqlserver://localhost\\DatabaseName=master;user=sa;password=Yibz6969";
-        // Connection conn;
-        // try {
-        // conn = DriverManager.getConnection(dbURL);
-        // } catch (SQLException e1) {
-        // e1.printStackTrace();
-        // }
-
-        // // if (conn != null) {
-        // System.out.println("Connected");
-        // }
-        // blInterface.userInterface userInt = businessLayer.blFactory.getUserObject();
-        // try {
-        // blInterface.userDetails u = new blInterface.userDetails("nushiPishi", 1,
-        // LocalDate.now(), "nushi@pishi.com",
-        // "yibz6969");
-        // userInt.addUser(u);
-        // userInt.removeUser(u.userID);
-        // System.out.println(u.Name);
-
-        // } catch (Exception e) {
-        // System.out.println(e.getMessage());
-        // }
+        blInterface.userDetails deets = new blInterface.userDetails("Nushinishu", 4, LocalDate.now().minusYears(20),
+                "nushi@pishimons.com", "pishipishi");
+        try {
+            System.out.println(user.removeUser(1));
+            // deets = user.getUserDetails();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
