1bfb29398f0fd725197fcfb6148eff9ff791f616
Nushirvan Naseer <nushirvannaseer@gmail.com>
2020-11-08 12:45:00 +0500

    implemented addUser, removeUser, getUserDetails, checkEmail/UserExists to get data from the db

--- src/businessLayer/tests/dummyDB2.java
+++ src/businessLayer/tests/dummyDB2.java
@@ -1,12 +1,19 @@
 package businessLayer.tests;
 
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.sql.Date;
 import java.util.concurrent.Callable;
+
+import javax.naming.spi.DirStateFactory.Result;
+
+import java.beans.Statement;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Types;
 import java.sql.DatabaseMetaData;
 import com.microsoft.sqlserver.jdbc.SQLServerDriver;
 
@@ -17,76 +24,94 @@ public class dummyDB2 implements dbaseInterface.userInterface {
     private Connection conn = null;
 
     public dummyDB2() {
-        try {
+        // try {
 
-            String dbURL = "jdbc:sqlserver://localhost\\appStore:1433";
-            String user = "sa";
-            String pass = "Yibz6969";
-            conn = DriverManager.getConnection(dbURL, user, pass);
-            if (conn != null) {
-                DatabaseMetaData dm = (DatabaseMetaData) conn.getMetaData();
-                System.out.println("Driver name: " + dm.getDriverName());
-                System.out.println("Driver version: " + dm.getDriverVersion());
-                System.out.println("Product name: " + dm.getDatabaseProductName());
-                System.out.println("Product version: " + dm.getDatabaseProductVersion());
-            }
+        // String dbURL = "jdbc:sqlserver://localhost\\appStore:1433";
+        // String user = "sa";
+        // String pass = "Yibz6969";
+        // conn = DriverManager.getConnection(dbURL, user, pass);
+        // if (conn != null) {
+        // DatabaseMetaData dm = (DatabaseMetaData) conn.getMetaData();
+        // System.out.println("Driver name: " + dm.getDriverName());
+        // System.out.println("Driver version: " + dm.getDriverVersion());
+        // System.out.println("Product name: " + dm.getDatabaseProductName());
+        // System.out.println("Product version: " + dm.getDatabaseProductVersion());
+        // }
 
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        } finally {
-            try {
-                if (conn != null && !conn.isClosed()) {
-                    conn.close();
-                }
-            } catch (SQLException ex) {
-                ex.printStackTrace();
-            }
-        }
+        // } catch (SQLException ex) {
+        // ex.printStackTrace();
+        // } finally {
+        // try {
+        // if (conn != null && !conn.isClosed()) {
+        // conn.close();
+        // }
+        // } catch (SQLException ex) {
+        // ex.printStackTrace();
+        // }
+        // }
 
     }
 
-    public void addUser(dbaseInterface.userDetails user) {
-        // if (user.Name == "nushiPishi")
-        // return;
-        // else
-        // throw new IllegalArgumentException("hello");
-
+    private void createConnection() {
+        String dbURL = "jdbc:sqlserver://localhost\\DatabaseName=appStore:1433";
+        String username = "sa";
+        String pass = "Yibz6969";
         try {
-            String dbURL = "jdbc:sqlserver://localhost\\appStore:1433";
-            String username = "sa";
-            String pass = "Yibz6969";
             conn = DriverManager.getConnection(dbURL, username, pass);
-            Date dob = Date.valueOf(user.DOB);
-            CallableStatement cs = conn.prepareCall("{call signup(?,?,?,?)}");
-            cs.setString(1, user.Name);
-            cs.setString(2, user.email);
-            cs.setString(3, user.password);
-            cs.setDate(4, dob);
-            // cs.setDate(4, dob);
-            cs.execute();
-
         } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            // System.out.println(e.getMessage());
-
             e.printStackTrace();
         }
+    }
 
+    public void addUser(dbaseInterface.userDetails user) throws SQLException {
+
+        this.createConnection();
+        Date dob = Date.valueOf(user.DOB);
+        CallableStatement cs = conn.prepareCall("{call appStore.dbo.add_user(?,?,?,?)}");
+        cs.setString(1, user.Name);
+        cs.setString(2, user.email);
+        cs.setString(3, user.password);
+        cs.setDate(4, dob);
+        cs.execute();
     }
 
     public userDetails getUserDetails(int userID) {
-        if (userID < 100)
-            return new userDetails("nushiPishi", 69, LocalDate.now(), "nushi@pishi.com", "yibz6969");
-        else
-            throw new IllegalArgumentException("alo jee error aya reee");
+        this.createConnection();
+        try {
+            CallableStatement cs;
+            userDetails resultObj = new userDetails();
+            cs = conn.prepareCall("{call appStore.dbo.get_User_Details(?)");
+            cs.setInt(1, userID);
+            ResultSet rs = cs.executeQuery();
+            if (rs != null) {
+                resultObj.userID = rs.getInt(1);
+                resultObj.Name = rs.getString(2);
+                resultObj.DOB = rs.getDate(3).toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+                resultObj.password = rs.getString(4);
+                resultObj.email = rs.getString(5);
+            }
+            return resultObj;
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return null;
+        }
+
     }
 
     // if user does not exist throw invalidargumentexception
     public void removeUser(int userID) {
-        if (userID == 69)
-            return;
-        else
-            throw new IllegalArgumentException("alo jee ik hor error aya jay");
+        this.createConnection();
+
+        CallableStatement cs;
+        try {
+            cs = conn.prepareCall("{call appStore.dbo.remove_user(?)}");
+            cs.setInt(1, userID);
+            cs.execute();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     // if user or app does not exists, throw invalidargumentexception
@@ -137,8 +162,25 @@ public class dummyDB2 implements dbaseInterface.userInterface {
 
     }
 
-    public boolean checkEmailExists(int userID, String email) {
-        return true;
+    public boolean checkEmailExists(String email) {
+        try {
+            this.createConnection();
+            CallableStatement cs = conn.prepareCall("{call appStore.dbo.check_email_exist(?,?)}");
+            cs.setString(1, email);
+            cs.registerOutParameter(2, Types.INTEGER);
+            cs.execute();
+
+            if (cs.getInt(2) == 1) {
+                return true;
+            }
+
+            return false;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+
     }
 
     // -1 if not installed
@@ -147,6 +189,23 @@ public class dummyDB2 implements dbaseInterface.userInterface {
     }
 
     public boolean checkUserExists(int userID) {
+        try {
+            this.createConnection();
+            CallableStatement cs = conn.prepareCall("{call appStore.dbo.check_User_Exist(?,?)}");
+            cs.setInt(1, userID);
+            cs.registerOutParameter(2, Types.INTEGER);
+            cs.execute();
+
+            if (cs.getInt(2) == 1) {
+                return true;
+            }
+
+            return false;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
         return false;
     }
 
