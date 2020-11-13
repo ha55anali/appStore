ecee0c463e301686752a8086a94eb1728f4bdcb0
Nushirvan Naseer <nushirvannaseer@gmail.com>
2020-11-14 01:20:06 +0500

    changed a few return types

--- src/dbaseInterface/devInterface.java
+++ src/dbaseInterface/devInterface.java
@@ -1,14 +1,13 @@
-package dbaseInterface;
+// package dbaseInterface;
 
 //assume all parameters passed are correct
-public interface devInterface
-{
+public interface devInterface {
     public userDetails getDevDetails(int devID);
 
     public boolean checkDevExists(int devID);
 
-    //appID will be assigned by database. pass dummy appID
-    public void addUser(userDetails dev);
+    // appID will be assigned by database. pass dummy appID
+    public int addUser(userDetails dev);
 
     public void removeUser(int devID);
 
@@ -16,9 +15,9 @@ public interface devInterface
 
     public void removeApp(int devID, int appID);
 
-    //return 1 if dev has made appID
+    // return 1 if dev has made appID
     public boolean checkAppDev(int devID, int appID);
 
-    //return 1 if email is being used by some dev
+    // return 1 if email is being used by some dev
     public boolean checkEmailExists(String email);
 }
